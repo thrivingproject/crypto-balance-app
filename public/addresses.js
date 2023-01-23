5f0ddbce9e0af84587aa80a0253bc19a4a3b043a
Christian Isaman <98602195+thrivingproject@users.noreply.github.com>
2023-01-23 08:46:54 -0400

    new velo/eth nft

--- public/addresses.js
+++ public/addresses.js
@@ -44,7 +44,7 @@ const BLOCKCHAINS = {
         wallets: [metamask],
         liquidity: {
             uniswap: {
-                nfts: [295341, 296386]
+                nfts: [295341, 296596]
             }
         },
         provider: 'https://opt-mainnet.g.alchemy.com/v2/',
